a5ecaf8d1c28767d14fa40626ea8782833fb2409
unknown <arifulislambipubd@gmail.com>
2021-12-29 13:57:41 +0600

    edit

--- js/script.js
+++ js/script.js
@@ -30,3 +30,29 @@ var mixer = mixitup('.mixItUpContent',{
 		effectsOut:'fade translateX(-100%)',
 	}
 });
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+//Back To top
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
